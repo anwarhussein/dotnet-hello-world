e51a21ca6f8075b15470e20e2684d5e63e213109
anwar <anwarhussein2353@gmail.com>
2021-10-25 20:34:06 -0500

    Completed C# and dotnet assignment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace dotnet_hello_world
 {
@@ -7,6 +8,27 @@ namespace dotnet_hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(lovefunc(2, 2));
+            Console.WriteLine(lovefunc(5, 2));
+            Object[] users = { 1, 4, 5, 6, 8, 6 };
+            int [] array = { 1, 4, 0, -5, 8, -2 };
+            Console.WriteLine(Check(users, 3));
+            Console.WriteLine(Check(users, 4));
+            Console.WriteLine(PositiveSum(array));
+        }
+
+        public static bool lovefunc(int flower1, int flower2)
+        {
+            return flower1 % 2 != flower2 % 2;
+        }
+        public static bool Check(object[] a, object x)
+        {
+            return Array.IndexOf(a, x) != -1;
+        }
+        public static int PositiveSum(int[] arr)
+        {
+            return arr.Sum(x => x > 0 ? x : 0);
         }
     }
+
 }
